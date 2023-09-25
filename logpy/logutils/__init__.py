91bdbfe5d1da69011348a051d4d8bd64d9991642
Muhammed Abdullah Shaikh <muhammed.shaikh@accoladeelectronics.com>
2023-09-25 18:40:15 +0530

    Added logtype for Network Module: 1. Network Switching 2. Pdp Deactivation 3. Network Registration

--- logpy/logutils/__init__.py
+++ logpy/logutils/__init__.py
@@ -8,6 +8,9 @@ from .network import (
     QISend,
     QMTPublish,
     QMTResponse,
+    NetSwitching,
+    PdpDeact,
+    NetRegistration,
 )
 from .sleep import (
     SleepPatterns,
@@ -31,6 +34,15 @@ def add_mqtt_logs(la: LogAnalyzer, p):
     la.add_log_type(QMTPublish("MQTT Publish", topics=p.topics, ignore_case=p.ignore_case))
     la.add_log_type(QMTResponse("MQTT Response", ignore_case=p.ignore_case))
 
+def add_netswithching_logs(la: LogAnalyzer, p):
+    la.add_log_type(NetSwitching("Net Switching", ignore_case=p.ignore_case))
+
+def add_pdpdeact_logs(la: LogAnalyzer, p):
+    la.add_log_type(PdpDeact("PDP Deactivation", ignore_case=p.ignore_case))
+
+def add_netreg_logs(la: LogAnalyzer, p):
+    la.add_log_type(NetRegistration("Net Registration", ignore_case=p.ignore_case))
+
 def add_sleep_logs(la: LogAnalyzer, p):
     la.add_log_type(Suspend("Going to Sleep", ignore_case=p.ignore_case))
     la.add_log_type(Active("Waking Up", ignore_case=p.ignore_case))
@@ -45,13 +57,13 @@ def add_cvp_storage_logs(la: LogAnalyzer, p):
     la.add_log_type(CVP("CVP", ignore_case=p.ignore_case, log_file_path=p.log_file))
 
 MODULE_SUBFUNCTIONS = {
-    "network": ["tcp", "mqtt"],
+    "network": ["tcp", "mqtt", "netswitching", "pdpdeact", "netregistration"],
     "sleep": ["ignition", "sleepcycle"],
     # "storage": ["ais140", "cvp"]
     }
 
 MODULE_ADDLOG = {
-    "network" : {"tcp": add_tcp_logs, "mqtt": add_mqtt_logs},
+    "network" : {"tcp": add_tcp_logs, "mqtt": add_mqtt_logs, "netswitching": add_netswithching_logs, "pdpdeact": add_pdpdeact_logs, "netregistration": add_netreg_logs},
     "sleep" : {"sleepcycle": add_sleep_logs, "ignition": add_ignition_logs},
     # "storage": {"ais140": add_ais_storage_logs, "cvp": add_cvp_storage_logs}
 }
