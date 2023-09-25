2059efc2b754408f4e116a9cde0e207b9b6ee095
Muhammed Abdullah Shaikh <muhammed.shaikh@accoladeelectronics.com>
2023-09-25 19:45:39 +0530

    Printing name in 1st column atleast once

--- logpy/logutils/logabc.py
+++ logpy/logutils/logabc.py
@@ -12,15 +12,17 @@ class _BasicLog:
 
     def print_summary(self, table, se):
         # se is show_empty
+        nCol1 = False # means name in column 1, set to true when 1st time name in columnn 1 is printed
         for k,v in self.result_dict.items():
             if isinstance(v, dict):
-                flag = 1
+                flag = 1 # this for nested dictionary within the value
                 for key, val in v.items():
                     if isinstance(val, dict):
                         # table.append([k, key, str(tabulate(val.items(), tablefmt="plain"))])
                         tbl_str = str(tabulate([(key,k1,v1) for (k1,v1) in val.items() if v1 or se], tablefmt="plain"))
                         if len(tbl_str) > 0 or se:
-                            table.append(["", k, tbl_str])
+                            table.append(["" if nCol1 else self.name, k, tbl_str])
+                            nCol1 = True
                         flag = 0
                     # else:
                     #     if not val:
@@ -29,10 +31,12 @@ class _BasicLog:
                 if flag:
                     tbl_str = str(tabulate([i for i in v.items() if i[1] or se], tablefmt="plain"))
                     if len(tbl_str) > 0 or se:
-                        table.append(["", k, tbl_str])
+                        table.append(["" if nCol1 else self.name, k, tbl_str])
+                        nCol1 = True
             else:
                 if v or se:
-                    table.append([self.name, k, v])
+                    table.append(["" if nCol1 else self.name, k, v])
+                    nCol1 = True
 
     def __call__(self, line):
         match = self.pattern.search(line)
@@ -48,11 +52,19 @@ class _BasicLog:
 class BasicLog(_BasicLog):
     def __init__(self, name, pattern, **kwargs):
         super(BasicLog, self).__init__(name, pattern, **kwargs)
-        # self.result_dict.Count = 0
+        self.writeToFile = kwargs.get("writeToFile", 0)
     
     def update_counts(self, match):
-        # self.result_dict.Count +=1
-        return 0
+        """
+        Update the counts based on the given match.
+
+        Args:
+            match (str): The match to update the counts with.
+
+        Returns:
+            int: 1 if want to write to file, else 0.
+        """
+        return self.writeToFile
     
     def print_summary(self, table, show_empty=False):
         return super().print_summary(table, show_empty)
